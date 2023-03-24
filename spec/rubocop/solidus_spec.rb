53b8640f74e72ef42d5bdc38308a7c2855db3592
piyushswain <piyush.swain3@gmail.com>
2023-03-24 15:12:27 +0530

    Initial Commit

---
+++ spec/rubocop/solidus_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::Solidus do
+  it "has a version number" do
+    expect(Rubocop::Solidus::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
