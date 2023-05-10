c9d99274c982ed5d338c375da34aa220415a538f
Michelle Crisologo <michelle.crisologo@gmail.com>
2023-05-10 12:58:00 +0800

    Add new Solidus/SpreeCalculatorPercentPerItemDeprecated cop

---
+++ lib/rubocop/cop/solidus/spree_calculator_percent_per_item_deprecated.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Solidus
+      # This cop finds Spree::Calculator::PercentPerItem calls.
+      # This cop is needed as they have been deprecated in future version.
+      #
+      class SpreeCalculatorPercentPerItemDeprecated < Base
+        extend AutoCorrector
+
+        MSG = 'Spree::Calculator::PercentPerItem is deprecated.'
+
+        def_node_matcher :percent_per_item?, <<~PATTERN
+          (send (... (... :Calculator) :PercentPerItem) $_)
+        PATTERN
+
+        def on_send(node)
+          percent_per_item?(node) do |method_used|
+            add_offense(node, message: MSG) do |corrector|
+              corrector.replace(node, "Spree::Calculator::PercentOnLineItem.#{method_used}")
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/solidus/spree_calculator_percent_per_item_deprecated_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Solidus::SpreeCalculatorPercentPerItemDeprecated, :config do
+  let(:config) { RuboCop::Config.new('Solidus/SpreeCalculatorPercentPerItemDeprecated' => { 'Enabled' => true }) }
+
+  context 'when calling #new' do
+    it 'registers an offense when using `#new`' do
+      expect_offense(<<~RUBY)
+        Spree::Calculator::PercentPerItem.new
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Spree::Calculator::PercentPerItem is deprecated.
+      RUBY
+    end
+
+    it 'does not register an offense when using `#new`' do
+      expect_no_offenses(<<~RUBY)
+        Spree::Calculator::PercentOnLineItem.new
+      RUBY
+    end
+  end
+
+  context 'when calling #create' do
+    it 'registers an offense when using `#create`' do
+      expect_offense(<<~RUBY)
+        Spree::Calculator::PercentPerItem.create
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Spree::Calculator::PercentPerItem is deprecated.
+      RUBY
+    end
+
+    it 'does not register an offense when using `#create`' do
+      expect_no_offenses(<<~RUBY)
+        Spree::Calculator::PercentOnLineItem.create
+      RUBY
+    end
+  end
+end
