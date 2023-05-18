d9987b789c40646e80ad91cf4dd20fc97c8c74e5
Michelle Crisologo <michelle.crisologo@gmail.com>
2023-05-19 01:53:30 +0800

    Add new Solidus/SpreeIconDeprecated cop

---
+++ lib/rubocop/cop/solidus/spree_icon_deprecated.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Solidus
+      # This cop finds icon helper calls and suggest using solidus_icon
+      #
+      # @example EnforcedStyle:
+      #   # bad
+      #   helper.icon('example')
+      #
+      #   # good
+      #   helper.solidus_icon('example')
+      #
+      #
+      class SpreeIconDeprecated < Base
+        extend AutoCorrector
+
+        MSG = 'In Solidus 2.3, `icon` helper has been deprecated in favor of `solidus_icon`'
+
+        RESTRICT_ON_SEND = %i[icon].freeze
+
+        def_node_matcher :icon?, <<~PATTERN
+          (send (send nil? ...) :icon ...)
+        PATTERN
+
+        def on_send(node)
+          return unless icon?(node)
+
+          add_offense(node) do |corrector|
+            corrector.replace(node, node.source.gsub('icon', 'solidus_icon'))
+          end
+        end
+      end
+    end
+  end
+end
